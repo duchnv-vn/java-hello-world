9c75bf201109bf79ea8250470b9e92cb4fe8e644
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-04-10 16:52:50 +0700

    Section 2: Deep to boolean, char, integer primitive types

---
+++ doc/section_2/primitive-data.md
@@ -0,0 +1,14 @@
+# PRIMITIVE DATA TYPES
+
+## Declare primitive variables
+
+- Syntax: `{type} {variable_name} = {value};`
+  - Primitive types:
+    - boolean (1 bit)
+    - char (2 bytes - 16 bits)
+    - byte (1 byte - 8 bits)
+    - short (2 bytes - 16 bits)
+    - int (4 bytes - 32 bits)
+    - long (8 bytes - 64 bits)
+    - float (4 bytes - 32 bits)
+    - double (8 bytes - 64 bits)
--- src/setion_2/main.java
+++ src/setion_2/main.java
@@ -4,10 +4,10 @@ class Main {
     public static void main(String[] args) {
         boolean booleanVar = true;
         booleanVar = false;
-        System.out.println("booleanVar:" + booleanVar);
+        System.out.println("booleanVar: " + booleanVar);
 
-        String textVar = "Hello world, JAVA";
-        System.out.println("textVar:" + textVar);
+        String stringVar = "Hello world, JAVA";
+        System.out.println("stringVar: " + stringVar);
 
         byte negativeByteVar = -128;
         byte positiveByteVar = 127;
@@ -16,21 +16,31 @@ class Main {
 
         short negativeShortVar = -32768;
         short positiveShortVar = 32767;
-        System.out.println("negativeShortVar:" + negativeShortVar);
-        System.out.println("positiveShortVar:" + positiveShortVar);
+        System.out.println("negativeShortVar: " + negativeShortVar);
+        System.out.println("positiveShortVar: " + positiveShortVar);
 
         int negativeIntVar = -2147483648;
         int positiveIntVar = 2147483647;
-        System.out.println("negativeInitVar:" + negativeIntVar);
-        System.out.println("positiveIntVar:" + positiveIntVar);
+        System.out.println("negativeInitVar: " + negativeIntVar);
+        System.out.println("positiveIntVar: " + positiveIntVar);
 
-        long longVar = 1111111111;
-        System.out.println("longVar:" + longVar);
+        long longVar = 999999999999999999L;
+        System.out.println("longVar: " + longVar);
 
-        float floatVar = 3.555999f;
-        System.out.println("floatVar:" + floatVar);
+        float floatVar = 3.555999F;
+        System.out.println("floatVar: " + floatVar);
 
         double doubleVar = 2.999111;
-        System.out.println("doubleVar:" + doubleVar);
+        System.out.println("doubleVar: " + doubleVar);
+
+        char lowerCaseCharVar = Character.toLowerCase('D');
+        System.out.println("lowerCaseCharVar: " + lowerCaseCharVar);
+
+        char digitCharVar = '1';
+        boolean isDigitValue = Character.isDigit(digitCharVar);
+        System.out.println("isDigitValue: " + isDigitValue);
+
+        char unicodeNumVar = 1;
+        System.out.println("unicodeNumVar: " + unicodeNumVar);
     }
 }
