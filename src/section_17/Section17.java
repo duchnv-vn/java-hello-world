a085ef3f1381d17b2c259b6bc5a42285a1e76792
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-05-12 15:51:12 +0700

    Section 17: arrayList

---
+++ src/section_17/Section17.java
@@ -0,0 +1,18 @@
+package section_17;
+
+import java.util.ArrayList;
+
+public class Section17 {
+    public static void main(String[] args) {
+        // LIST COLLECTION
+        // ARRAY LIST
+        ArrayList<String> countryNames = new ArrayList<String>();
+        countryNames.add("Vietnam");
+        countryNames.addFirst("USA");
+        countryNames.addLast("Japan");
+        countryNames.remove("Japan");
+        countryNames.removeLast();
+
+        // LINKED LIST
+    }
+}
