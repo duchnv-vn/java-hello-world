c70cc124e58b3e141a7b56e796244010ea279602
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-05-25 17:02:31 +0700

    Section 20: Hashmap

---
+++ src/section_20/Section2.java
@@ -0,0 +1,16 @@
+package section_20;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Section2 {
+    public static void main(String[] args) {
+        // INITIATION & DECLARATION
+        Map<String, String> countryMap = new HashMap<>();
+        var countryMap2 = new HashMap<String, String>();
+        countryMap2.put("usa", "Washington DC");
+        countryMap2.put("vietnam", "Ha Noi");
+        countryMap2.put("india", "New Delhi");
+
+    }
+}
