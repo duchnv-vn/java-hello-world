927d22ef1e34cf2bc7a30f28c4920c32d50fe772
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-06-07 16:40:35 +0700

    Section 26: Stream map & flatMap

--- src/section_26/Section26.java
+++ src/section_26/Section26.java
@@ -11,13 +11,25 @@ public class Section26 {
 
         String[] words = { "aaa", "bbb", "ccc" };
         var wordsStream = Arrays.stream(words);
+        // wordsStream.map(word -> word.toUpperCase()); // can not chain multiple times
+        // wordsStream.forEach(System.out::println); // can not chain multiple times
+        var words2 = wordsStream.toList();
+        System.out.println("words2: " + words2);
 
         // EMPTY STREAM
         var emptyStream = Stream.empty();
 
         // 3 STEPS OF STREAM PROCESS
-        // 1. Create a stream: stream() | parallelStream() | Stream.of() | Arrays.stream()
-        // 2. Add one or more intermediate operations - for transform initial data source into other
+        // 1. Create a stream: stream() | parallelStream() | Stream.of() |
+        // Arrays.stream()
+        // 2. Add one or more intermediate operations - for transform initial data
+        // source into other
         // 3. Apply terminal operation to produce result
+
+        // FLAT MAP
+        String[] fullName = { "Duc", "Huynh" };
+        var letters = Stream.of(fullName).map(word -> word.split("")).flatMap(Arrays::stream)
+                .map(word -> word.toLowerCase()).toList();
+        ;
     }
 }
